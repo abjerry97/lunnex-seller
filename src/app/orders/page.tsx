484712a53087d8a6398671d38be0b38b6c7bc664
Abjerry97 <abj97x@gmail.com>
2023-10-14 10:01:58 +0100

    ww

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -7,25 +7,25 @@ export default function Orders() {
     <DashboardLayout>
       <div className="border-b-2 mb-4">
         <ul className="flex gap-4 text-xs font-semibold mb-2">
-          <li className=" hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
+          <li className="text-xs hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
             <span> ALL</span>{" "}
             <span className="py-0.5 px-2 bg-[#26264C] h-fit text-white text-xs rounded-xl font-light">
               123
             </span>
           </li>
-          <li className=" hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
+          <li className="text-xs  hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
             <span> NEW</span>{" "}
             <span className="py-0.5 px-2 bg-[#7979F1] h-fit text-white text-xs rounded-xl  font-light">
               123
             </span>
           </li>
-          <li className=" hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
+          <li className="text-xs  hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
             <span> DELIVERED</span>{" "}
             <span className="py-0.5 px-2 bg-[#31D0AA] h-fit text-black text-xs rounded-xl  font-light">
               123
             </span>
           </li>
-          <li className=" hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
+          <li className="text-xs  hover:opacity-100 cursor-pointer flex items-center gap-1 justify-center">
             <span> CANCELLED</span>{" "}
             <span className="py-0.5 px-2 bg-[#D03131] h-fit text-white text-xs rounded-xl  font-light">
               123
