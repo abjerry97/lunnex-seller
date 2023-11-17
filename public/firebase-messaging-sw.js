a0543c43cdad25b3695e9f8f9ccb8d56ff7e95bd
Abjerry97 <abj97x@gmail.com>
2023-11-17 10:08:35 +0100

    fis

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,31 +1,48 @@
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+function requestPermission() {
+  console.log('Requesting permission...');
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+    }
+  });
+}
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  }).catch(function(err) {
+    console.log('Service worker registration failed, error:', err);
+  });
+}
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-  apiKey: "AIzaSyDPtpEIiYd5a8Og1KaD6wq-OBPhjW04gS8",
-  authDomain: "avid-infinity-245609.firebaseapp.com",
-  projectId: "avid-infinity-245609",
-  storageBucket: "avid-infinity-245609.appspot.com",
-  messagingSenderId: "924884594067",
-  appId: "G-5VL7P7C8DP",
-//   measurementId: 'G-measurement-id',
-};
+// // Scripts for firebase and firebase messaging
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-firebase.initializeApp(firebaseConfig);
+// // Initialize the Firebase app in the service worker by passing the generated config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDPtpEIiYd5a8Og1KaD6wq-OBPhjW04gS8",
+//   authDomain: "avid-infinity-245609.firebaseapp.com",
+//   projectId: "avid-infinity-245609",
+//   storageBucket: "avid-infinity-245609.appspot.com",
+//   messagingSenderId: "924884594067",
+//   appId: "G-5VL7P7C8DP",
+// //   measurementId: 'G-measurement-id',
+// };
 
-// Retrieve firebase messaging
-const messaging = firebase.messaging();
+// firebase.initializeApp(firebaseConfig);
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+// // Retrieve firebase messaging
+// const messaging = firebase.messaging();
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
+// messaging.onBackgroundMessage(function(payload) {
+//   console.log('Received background message ', payload);
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   };
+
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
